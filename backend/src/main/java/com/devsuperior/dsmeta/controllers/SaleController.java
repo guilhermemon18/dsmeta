bbd42a2b5beaad54bdd358a70b4382958f992910
guilhermemon18 <guilhermemonsani7@gmail.com>
2022-07-13 21:22:36 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -20,9 +21,15 @@ public class SaleController {
 	private SaleService service;
 	
 	//configura a função para responder ao protocolo http.
-	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	@GetMapping//requisição get.
+	public Page<Sale> findSales(
+			//@RequestParam parametros que são passados na busca
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="")String maxDate,
+			Pageable pageable){//Pageable pageable: pagina em páginas de 20 elementos, 
+		//precisa declarar no controller e no service.
+		//dados trafegam como texto na web, por isso String nas datas.
+		return service.findSales(minDate,maxDate,pageable);
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
@@ -8,4 +13,9 @@ import com.devsuperior.dsmeta.entities.Sale;
 //JpaRepository<T1,T2> onde T1 é o tipo da entidade e T2 é o tipo da chave da entidade.
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
+	//consulta customizada no banco de dados, pela data máxima e data mínima, comando em JPQL.
+	//:min é LocalDate min e :max é LocalDate max.
+	//ordena pelas maiores vendas.
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
